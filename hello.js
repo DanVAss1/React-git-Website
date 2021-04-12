56c0746c27734218372818f58d87c04d29dc93b5
Daniel Vass <daniel.vass@bp.com>
2021-04-12 15:36:49 +0100

    mac main

--- hello.js
+++ hello.js
@@ -1,1 +1,73 @@
-//hello
+import React from 'react';
+import './App.css';
+import picture from "./Japan.jpg";
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+class ToDoForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>Hello</h1>
+        <p>Enter your name:</p>
+        <input
+          type="text"
+        />
+      </form>
+    );
+  }
+}
+function Header() {
+  return(
+    <div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    </div>
+  )
+  }
+
+function Main() {
+  return (
+    <section>
+      <p>here is a picture</p>
+      <img src={picture} height={500} alt="mount"/>
+      <Button variant="primary" size="lg" block> Block level button
+  </Button>
+    </section>
+  )
+}
+function App() {
+  return (
+    <div className="Apps">
+      <Header />
+      <ToDoForm />
+      <Main />
+    </div>
+  );
+  }
+  
+export default App;
